6f9cb05db235792b32b7175171591bc5ad6c9ced
Bhaskar p <152620718+Bhaskarp2107@users.noreply.github.com>
2026-08-16 15:02:46 +0530

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
-
+  baseURL: 'https://reachinbox-scheduler-ms2m.onrender.com/api',
   withCredentials: true,
-
   headers: {
     'Content-Type': 'application/json',
   },
